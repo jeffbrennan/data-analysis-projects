953a639776dd0fe6c3e647575e8351f0d99ea7d2
jeffbrennan <jeffbrennan10@gmail.com>
2020-01-20 15:59:03 -0600

    initial commit

---
+++ htown-mayoral/readme.md
@@ -0,0 +1,29 @@
+# Houston Mayoral Election 2019 - Scraping & Analysis
+
+
+## Motivation
+
+The 2019 Houston Mayoral Election took place on November 5, 2019, wherein incumbent Sylvester Turner faced challenger Tony Buzbee, a local trial lawyer. Other issues on the ballot included parity between police officer and firefighter pay, funding for flooding infrastructure and public transportation, and a number of local positions. 
+
+Harrisvotes.com published results from this election on their website shortly thereafter, including the voting distribution by precinct. Since different precincts in Houston have vastly different populations and economic interests, I thought it would be interesting to  identify voting trends based on a number of these unique precinct differences.
+ 
+
+### Challenges
+
+Unfortunately, harrisvotes did not offer an easy way to download this data as a spreadsheet. Moreover, they publish all of their results to one page. Since there has been another election since November, all of it was condensed into a single .pdf.
+
+
+Completed:
+* [x] Scraping the harrisvotes dynamically generated data
+
+Forthcoming:
+* [ ] Getting precinct level data from the US Census
+* [ ] Visualizing differences in precincts
+* [ ] Building models that can infer voting trends 
+
+
+### Resources 
+
+* Scraping: Python (selenium, requests); archive.org
+* Visualization: R, ArcMap
+* Analysis: R
---
+++ htown-mayoral/scrape.py
@@ -0,0 +1,50 @@
+import requests, json
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+# loads in headless firefox browser to get dynamically generated contests
+def selenium_gen():
+    binary = FirefoxBinary(r'C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(firefox_binary=binary, options=options,
+                               executable_path = r'C:\Users\jeffb\software\geckodriver.exe')
+    return driver
+
+# TODO: get contest name in addition to ID
+# using the contestPanel- id prefix, get the corresponding contest number
+def get_contests():
+    contest_ids = []  
+    driver = selenium_gen()
+    driver.get('https://web.archive.org/web/20191116202735/https://www.harrisvotes.com/ElectionResults/ElectionDay')
+    
+    contests = driver.find_elements_by_xpath('//div[contains(@id, "contestPanel-")]')
+
+    for contest in contests:
+        contest_label = contest.get_attribute('id')
+        contest_ids.append((contest_label.split('-'))[1])
+    driver.quit()
+
+    return contest_ids
+
+# using contest number from get_contests(), access the corresponding .json file and save locally
+# local saving reduces total number of requests (lowering chance of getting blacklisted)
+def get_data(contest_ids): 
+    for id in contest_ids:
+        print(id)
+        response = requests.get('http://harrisvotes.com/Data/1119/pv-' + id + '.json')
+        result = json.loads(response.content)
+        response.close()
+
+        with open('data/contests/' + id + '.json', 'w') as f:
+            json.dump(result, f, ensure_ascii=False)
+
+def main():
+    contest_ids = get_contests()
+
+    print('Getting Contest Data...')
+    get_data(contest_ids)
+
+if __name__ == '__main__':
+    main()
